aadb5176efb346965c26442a6e979d0796b1cc2a
Miko Clark Cruz <110460613+Mikouxei@users.noreply.github.com>
2022-08-25 15:32:17 +0800

    Update Character.java

--- class_with_constructor/Character.java
+++ class_with_constructor/Character.java
@@ -1,1 +1,26 @@
+public class Character {
+    // Attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
 
+    // Method
+    public void sayMyName() {
+        System.out.println("\nHello I am " + name + "!");
+    }
+
+    public void sayMyAttributes() {
+        System.out.println("My Strength is " + strength);
+        System.out.println("My Agility is " + agility);
+        System.out.println("My Intelligence is " + intelligence);
+    }
+
+    // Constructor
+    public Character(int str, int agi, int intel, String characterName) {
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        name = characterName;
+    }
+}
