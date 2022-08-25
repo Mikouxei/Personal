c5a37f4e0f69f4bd4231ae09307a7f409c3f0084
Miko Clark Cruz <110460613+Mikouxei@users.noreply.github.com>
2022-08-25 15:34:33 +0800

    Update App.java

--- class_with_constructor/App.java
+++ class_with_constructor/App.java
@@ -3,31 +3,31 @@ public class App {
 
         // Creates a new object named "Knight" from Character class
         Character knight = new Character(10, 5, 0, "Chen");
-        // Calls the sayMyName method
+        // Calls the sayMyName and sayMyAttributes method
         knight.sayMyName();
         knight.sayMyAttributes();
 
         // Creates a new object named "mage" from Character class
         Character mage = new Character(0, 5, 10, "Akasha");
-        // Calls the sayMyName method
+        // Calls the sayMyName and sayMyAttributes method
         mage.sayMyName();
         mage.sayMyAttributes();
 
         // Creates a new object named "thief" from Character class
         Character thief = new Character(5, 10, 0, "Gondar");
-        // Calls the sayMyName method
+        // Calls the sayMyName and sayMyAttributes method
         thief.sayMyName();
         thief.sayMyAttributes();
 
         // Creates a new object named "archer" from Character class
         Character archer = new Character(3, 6, 4, "Azami");
-        // Calls the sayMyName method
+        // Calls the sayMyName and sayMyAttributes method
         archer.sayMyName();
         archer.sayMyAttributes();
 
         // Creates a new object named "priest" from Character class
         Character priest = new Character(6, 2, 8, "Maxi");
-        // Calls the sayMyName method
+        // Calls the sayMyName and sayMyAttributes method
         priest.sayMyName();
         priest.sayMyAttributes();
 
